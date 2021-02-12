7f614785121bb0e1871c1cb3b86eacdc217eed38
gracekogithub <graceko02011972@gmail.com>
2021-02-12 10:57:29 -0800

    working on while looping for one player

--- RPSLS/GameSpace.cs
+++ RPSLS/GameSpace.cs
@@ -9,22 +9,26 @@ namespace RPSLS
     class GameSpace
     {
         //variables
+        public string inputUser;
         public List<HumanPlayers> personN;
         public AIplayer computer;
         //public string[] gesture;
         public List<GestureOption> gestureN;
 
         public int winningScore;
+        public int loosingScore;
         public int tie;
         public bool playingAgain;
         //constructor
         public GameSpace()
         {
+            inputUser = inputUser;
             personN = new List<HumanPlayers>();
             computer = new AIplayer("");
             gestureN = new List<GestureOption>();
       
             winningScore = 0;
+            loosingScore = 0;
             tie = 0;
             playingAgain = true;
 
@@ -47,30 +51,49 @@ namespace RPSLS
                               "Scissors decapitates Lizard\n"+
                               "Lizard eats Paper\n"+
                               "Paper disproves Spock\n"+
-                              "Spock vaporizes Rock\bNow, please choose your gestion option\'Rock,Paper,Scissors,Lizard, or Spock\'");
+                              "Spock vaporizes Rock\b" +
+                              "Now, please choose your gestion option\'Rock,Paper,Scissors,Lizard, or Spock\'");
         }
         public void ChooseGesture()
         {
-            Console.WriteLine($"Please, {personN[0].person} chose {gestureN[0].gesture}");
-            
-            
-            
-
-            //if ("JohnPickGesture" == "Rock")
-            //{
-            //    if (userInput == "rock")
-            //    {
-            //        Console.WriteLine("tie");
-            //    }
-            //    if (userInput == "Paper")
-            //    {
-            //        Console.WriteLine("You lost");
-            //        winningScore++;
-            //    }
-            //    Console.WriteLine();
-                
-            //}
-            
+            //Console.WriteLine($"Please, {personN[0].person} chose {gestureN[0].gesture}");
+            while (playingAgain)
+            {
+                //personN = "chose Rock";
+                Console.WriteLine("John chose Rock");
+                if (inputUser == "Rock")
+                {
+                    Console.WriteLine("tie");
+                }
+                else if (inputUser == "Paper")
+                {
+                    Console.WriteLine("You win");
+                    winningScore++;
+                }
+                else if (inputUser == "Scissors")
+                {
+                    Console.WriteLine("You lose");
+                    loosingScore++;
+                }
+                else if (inputUser == "Lizard")
+                {
+                    Console.WriteLine("You win");
+                    winningScore++;
+                }
+                else if (inputUser == "Spock")
+                {
+                    Console.WriteLine("You lose");
+                    loosingScore++;
+                }
+            }
+           
+           //if("rock" == "rock" || "sicssors" == "sicssor" ||"paper" == "paper" || "lizard" == "lizard "|| "spock" == "spock")
+           // {
+           //     Console.WriteLine("tie");
+           //     tie = 0;
+           //     playingAgain = true;
+           // }
+          
         }
 
 
